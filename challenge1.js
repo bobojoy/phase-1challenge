e45186285c04062686373297865494ee823db067
Joy Wambui <joy.wambui@student.moringaschool.com>
2024-07-01 16:13:55 +0300

    initial commit

--- challenge1.js
+++ challenge1.js
@@ -1,25 +1,28 @@
 function calculateGrade(){
+    let marks = prompt("Enter your marks here: ")
     let grade;
-    if (marks > 79){
-        grade = 'A';
+    if ( isNaN(marks) || marks > 100 || marks < 0) {
+        //If the marks are not within the valid range,return an error message
+        return "Invalid input.Please enter a number between 0 and 100.";
+    }
+    else if (marks > 79){
+       return  grade = 'A';
     }else if (marks >= 60){
-        grade = 'B';
+        return grade = 'B';
     }else if (marks >= 50){
-        grade = 'C';
+        return grade = 'C';
     }else if (marks >= 40){
-        grade = 'D';
+        return grade = 'D';
     }else {
-        grade = 'E';
+        return grade = 'E';
     }
 
 }
 
 //Return the grade if it has been assigned
-if (grade){
-    return grade;
-}
+//if (grade){
+    //return grade;}
 
-//If the marks are not within the valid range,return an error message
-return "Invalid input.Please enter a number between 0 and 100.";
 
-console.log(calculateGrade(23));
+
+console.log(calculateGrade(23));    
